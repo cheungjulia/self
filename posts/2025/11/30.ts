4198791beca1a2fd92a951ba1cb5b9485acd9ebb
julia writes <126901528+cheungjulia@users.noreply.github.com>
2025-11-30 17:46:10 +0800

    Create 30.ts

---
+++ posts/2025/11/30.ts
@@ -0,0 +1,8 @@
+import type { RawPost } from "@/lib/blog-data"
+
+export const post: RawPost = {
+  title: "robots in the wild",
+  location: "on a taxi in shenzhen heading back to hk through futian",
+  content: `massive traffic, and my dad chuckles to himself and says "must be a self driving car glitching out". he turns to my mum: "must be trained on data from the way you drive".
+more and more i believe learning only comes from bots being thrown into the wild. not from cramming knowledge into a brain.`,
+}
