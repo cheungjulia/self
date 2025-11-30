7fcffcf0d559f448b3a269cf94dbd9cdcc32be03
julia writes <126901528+cheungjulia@users.noreply.github.com>
2025-11-30 17:47:17 +0800

    Update 30.ts add sources

--- posts/2025/11/30.ts
+++ posts/2025/11/30.ts
@@ -5,4 +5,5 @@ export const post: RawPost = {
   location: "on a taxi in shenzhen heading back to hk through futian",
   content: `massive traffic, and my dad chuckles to himself and says "must be a self driving car glitching out". he turns to my mum: "must be trained on data from the way you drive".
 more and more i believe learning only comes from bots being thrown into the wild. not from cramming knowledge into a brain.`,
+sources: ["https://x.com/iliraliu_/status/1994482263031992778?s=46&t=8ZG-I97lF81q1B3TVPOpgw"]
 }
