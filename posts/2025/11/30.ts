ba14cb200e65b672af7d7f64c7fdb3a8656ca639
julia writes <126901528+cheungjulia@users.noreply.github.com>
2025-11-30 17:51:33 +0800

    Update 30.ts

--- posts/2025/11/30.ts
+++ posts/2025/11/30.ts
@@ -4,6 +4,9 @@ export const post: RawPost = {
   title: "robots in the wild",
   location: "on a taxi in shenzhen heading back to hk through futian",
   content: `massive traffic, and my dad chuckles to himself and says "must be a self driving car glitching out". he turns to my mum: "must be trained on data from the way you drive".
-more and more i believe learning only comes from bots being thrown into the wild. not from cramming knowledge into a brain.`,
-sources: ["https://x.com/iliraliu_/status/1994482263031992778?s=46&t=8ZG-I97lF81q1B3TVPOpgw"]
+more and more i believe learning only comes from bots being thrown into the wild. not from cramming knowledge into a brain.
+thinking about the "long tail" of edge cases that self driving cars need to be trained on. shenzhen... the way folks drive here... i know baidu is working on self driving car, but im skeptical given the way traffic works here. will monitor progress diligently`,
+sources: ["https://x.com/iliraliu_/status/1994482263031992778?s=46&t=8ZG-I97lF81q1B3TVPOpgw",
+"https://x.com/stefanfschubert/status/1994087153626743267?s=46&t=8ZG-I97lF81q1B3TVPOpgw",
+"https://x.com/waymo/status/1991989888778973280?s=46&t=8ZG-I97lF81q1B3TVPOpgw"]
 }
