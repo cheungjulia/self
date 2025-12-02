2d0a70a1119366fd1e8b55aeeab4e413097353dc
julia writes <126901528+cheungjulia@users.noreply.github.com>
2025-12-03 06:49:58 +0800

    Update 2.ts

--- posts/2025/12/2.ts
+++ posts/2025/12/2.ts
@@ -10,9 +10,11 @@ export const post: RawPost = {
   - 'Fragility, and the culture it creates, can be an asset in inspiring the sort of care necessary for the long term. A system that seeks the indestructible or infallible has the potential to encourage overconfidence, nonchalance, and the ultimate enemy of all archives, neglect.'
   - 'The success of century-scale storage comes down to the same thing that storage and preservation of any duration does: maintenance. The everyday work of a human being caring for something. [...] How it is stored will evolve or change as it is maintained, but if there are maintainers, it will persist.'
 
-  as someone who likes to document everything, but rarely goes back to read it, i often question what the value of saving something is. search requires you to remember what to search for. scrolling requires time. id be curious to fine-tune an LLM with all my journal entries, notes, images, thoughts, to see whether it can evolve into a mini-me.
+  as someone who likes to document everything, but rarely goes back to read it, i often question what the value of preservation is. search requires you to remember what to search for. scrolling requires time. all the bits of myself floating in the digital void. id be curious to fine-tune an LLM with all my journal entries, notes, images, thoughts, to see whether it can evolve into a mini-me.
   `,
   sources: [
     "https://killedbygoogle.com/",
+"https://lil.law.harvard.edu/blog/2025/08/20/live-and-let-die-rethinking-personal-digital-archiving-memory-and-forgetting-through-a-library-lens/",
+"https://www.nytimes.com/2022/12/31/technology/cloud-data-storage-google-apple-meta.html"
   ],
 }
