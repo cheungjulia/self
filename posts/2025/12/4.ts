d6674682c7803d770ae74f71968a20cbcff378fa
Julia Cheung <julia_cheung@mckinsey.com>
2025-12-06 08:37:29 +0800

    6th dec

--- posts/2025/12/4.ts
+++ posts/2025/12/4.ts
@@ -4,8 +4,15 @@ export const post: RawPost = {
   title: "words don't come easy to me -- especially in the LLM era",
   location: "early morning, cold room",
   content: `
-  i no longer bother to write in full sentences, nor do i even attempt to express myself artically. i dont even fix my typos anymore, when talking to LLMs. 
+  when talking to LLMs, i no longer bother to write in full sentences, nor do i even attempt to express myself artically. i dont even fix my typos. even before i get stuck it spoonfeeds me suggestions on the next possible word. more and more, reading non-LLM articles feel exhausting and my attention careens. 
     <img src="/2025-12-4.png" alt="words don't come easy" />
+
+    but i can tell. i cant tell if an article is necessarily LLM-generated, but i can tell when it's not. 
+
+    for the past decades, literacy rates were a part of measuring a country's level of development. in the LLM era, why do words still matter?
+    - <b>"Humans achieve agency by composing their lives in their own language."</b>: LLMs produce words, but not meaning. meaning comes when the words are your own, able to produce new experiences and ideas and "activates fresh existence" when spoken to someone else, to yourself, to some god. words form narrative and empowerment. to expand your vocab with new imagery, metaphors, is to allow you to explicate how you feel, what you think and believe more precisely. you build relationships with your words and they form a personal understanding with you. it's like when we used to learn a new SAT word and would stuff it into sentences. inorganic at first, but eventually you'd gain more nuance on how the word fits into your expression.
+    - <b>words as historical artifacts</b>: words aren't just words, they are a history. as someone who grew up in hong kong speaking english as my first language, i recognise my relationship with language stems from a colonial legacy, a british empire. with LLMs, history is conflated into a monolithic archive of training data. as Alain Mabanckou asks: "AI may well be able to take on board these cultural elements, but can it also reproduce the suffering of these oppressed peoples?"
+    - <b>beauty, resonance, aesthetic:</b> i write poetry. a part of poetry is surprise, to draw an unfamiliar connection between two things (within the constraint of what 'makes sense', however that's defined, but even this can be challenged e.g. by surrealists) -- the churning washing machine and the cycle of poverty, the moon and a belly button. LLMs predict predictable predictions. the next most probable token. words are not just tools, but things that draw beauty. 
   `,
   sources: [
     "https://thebaffler.com/odds-and-ends/brain-rot-without-borders-forum",
