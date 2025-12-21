9938794d14666b9f40cc3f613d9f44ff31d4a975
Julia Cheung <julia_cheung@mckinsey.com>
2025-12-21 09:59:11 +0800

    mew post 21 12 25

--- posts/2025/12/21.ts
+++ posts/2025/12/21.ts
@@ -9,13 +9,15 @@ export const post: RawPost = {
 
   a part of this reminds me of the macartney embassy to china in 1793, during the late qing dynasty -- bringing in watches and clocks that the dynasty was neither surprised by nor interested in (tho this was right before the industrial revolution and opium wars. life would have been very different if they established diplomatic relations). there's a history of international states being blind to / underestimating china. ofc a big part of it are the protectionist policies + decoupling + china-us relations and media portrayals disincentivising foreign visitors. 
 
-  this month i visited zhangjiajie, zhuhai, and frequent shenzhen. one main observation (from my narrow point of view) is that, unlike US, tech is for everyone. i made a list of "unassuming tech" that ive observed during my few visits:
+  this month i visited zhangjiajie, zhuhai, and frequent shenzhen. one main observation (from my narrow point of view) is that tech is for everyone and surfaces in different forms. i made a list of "unassuming tech" that ive observed during my few visits:
   - <b>rideshare pickup with computer vision:</b> detecting car plates and where each car is pulling up, shown on multipe screens so passengers know where to go (zhuhai)
   - <b>cleaning bots:</b> on roads, malls, streets, train stations -- it feels like a natural part of the city, almost so much so that i dont take note of it (shenzhen).
   - <b>exoskeleton legs for hiking and elderly:</b> tried this after hiking for 5 hours on a hurt ankle, and wow i felt light as a feather. it's meant for elderly and hurt, but they beta released this to big hiking spots in the country (zhangjiajie).
   - <b>drink fridges (not just vending machines):</b> where you can unlock with your phone, grab your drink, and leave -- it auto-detects what drinks youve taken (zhangjiajie)
   - <b>limited smartwatches for kids (xiao tian cai):</b> noticed a lot of kids wearing these -- it has location tracking, can create communication channels with two-way consented individuals (e.g. parents), stripped down version of alipay, little camera (saw twins taking photos of trees with it!) 
   - and then ofc <b>facial recognition, phone integration, and ID linkage:</b> every ticket into a tourist location linked to your identity, cameras at danger spots (e.g. 10km into a hike, or at a hard crossing in via ferrata), entering spots with only your face (zhangjiajie). side note -- toilets everywhere!
+  - <b> niche machines:</b> ceramic pot to roast sweet potatoes with a thermometer outside, and racks lining the side. sugarcane chopper machine. there's a product for everything (and can be found on taobao for pretty cheap!)
+  -<b> and then ofc LLMs!</b>: angry tourist on the train speaking to a worker, uses LLM (i think doubao from over the shoulder peeping) for translation and assistance.
   `,
   sources: [
     "https://www.chathamhouse.org/publications/the-world-today/2025-12/can-west-recover-chinas-hi-tech-knockout-blow",
@@ -27,8 +29,7 @@ export const post: RawPost = {
     "https://edition.cnn.com/travel/robotic-exoskeleton-hiking-china-intl-hnk",
     "Kenqing: https://www.kenqingkeji.com/product_details/27.html",
     "https://www.okii.com/html/en/product.html",
-    "https://www.reddit.com/r/chinalife/comments/1l8gzbe/smart_watches_for_kids/"
-
-
+    "https://www.reddit.com/r/chinalife/comments/1l8gzbe/smart_watches_for_kids/",
+    "https://www.alibaba.com/showroom/sugar-cane-cutting-machine.html"
   ],
 }
